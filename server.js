a661115a7e5d9069a7ed9b5a0610a19e29b2311e
Bandarusuryapranay <bandarusuryapranay1824@gmail.com>
2026-01-19 23:19:29 +0530

    Add CORS for Vercel frontend

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-
-app.use(cors({
-  origin: "https://cmas-frontend.vercel.app/",
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
